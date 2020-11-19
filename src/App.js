583ae409cc74806f48ff51d338df7d9a139ed59b
CinFrei <design@cinfrei.de>
2020-11-19 14:31:46 +0100

    Add FAB to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import styled from 'styled-components'
 import BookList from './components/BookList'
+import FloatingActionButton from './components/FloatingActionButton'
 
 export default function App() {
   return (
     <StyledApp>
       <h1>Book Owls</h1>
       <BookList listName="Bücherregal" />
+      <FloatingActionButton />
     </StyledApp>
   )
 }
 
 const StyledApp = styled.div`
-  font-family: sans-serif;
-  text-align: center;
+  position: relative;
 
   h1 {
+    margin: 0px;
     color: var(--dark-blue);
+    text-align: center;
   }
 `
