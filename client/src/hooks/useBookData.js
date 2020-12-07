cdde9d685c6c3a9ae2bab70cbe73ea7f5c09cc74
CinFrei <design@cinfrei.de>
2020-12-07 10:34:22 +0100

    Catch errors

--- client/src/hooks/useBookData.js
+++ client/src/hooks/useBookData.js
@@ -46,6 +46,18 @@ export default function useBookData() {
       .then((data) => {
         setResult(data.data)
       })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(error.response.data)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+        } else if (error.request) {
+          console.log(error.request)
+        } else {
+          console.log('Error', error.message)
+        }
+        console.log(error.config)
+      })
   }
 
   function toggleSearchBooksModal() {
--- express-api/routes/searchBooks.js
+++ express-api/routes/searchBooks.js
@@ -19,4 +19,16 @@ function handleSubmit(req, res) {
     .then((data) => {
       res.send(data.data.items)
     })
+    .catch(function (error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
 }
