a9a9c256af4257d9c1568ff523e70c1782d4ff24
CinFrei <design@cinfrei.de>
2020-12-11 16:15:57 +0100

    Delete old files

--- client/src/components/FloatingActionButton.test.js
+++
@@ -1,9 +0,0 @@
-import FloatingActionButton from './FloatingActionButton'
-import { render } from '@testing-library/react'
-
-describe('FloatingActionButton', () => {
-  it('renders correctly', () => {
-    const { container } = render(<FloatingActionButton />)
-    expect(container.firstChild).toMatchSnapshot()
-  })
-})
