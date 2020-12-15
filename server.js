6e38980093fda4eeda368d3948425a7f01e48663
CinFrei <design@cinfrei.de>
2020-12-15 15:15:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -34,10 +34,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
-/* app.get('*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
- */
+
 app.use(function (req, res, next) {
   next(createError(404))
 })
